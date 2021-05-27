0f8750adb2bad8b5f05a209f934fc74dc1336224
unknown <sumbulrafeeq@gmail.com>
2021-05-27 12:02:07 +0500

    Updated script for keeping styling intact after submit button is clicked

--- JS Variables/DocumentObjectModel/app.js
+++ JS Variables/DocumentObjectModel/app.js
@@ -1,12 +1,13 @@
 function submit()
 {
-    let inputElement  = document.querySelector("input");
+    let inputElement  = document.querySelector("#name");
     
     let inputValue = inputElement.value
-    let heading = document.querySelector("h1").innerHTML;
+    let heading = document.querySelector("h1>span").innerHTML=inputValue;
     
-    document.write (heading + inputValue);
-    alert("The message has been sent on the given number");
+    // document.write (heading + inputValue);
+    alert("The message has been sent on the given number")
 }
 
+// let a = prompt("What is ur color?")
 
